0def693f2ea536a439f38fbc1a04b60aee86c83c
ShrikantaMazumder <shrikantamazumder@gmail.com>
2021-01-13 16:45:43 +0600

    bulk update

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,58 +10,76 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-            return Card(
-              child: Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 15,
-                    ),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                      color: Theme.of(context).primaryColor,
-                      width: 2,
-                    )),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      '\$${userTransaction[index].amount.toStringAsFixed(2)}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                    ),
+      child: userTransaction.isEmpty
+          ? Column(
+              children: [
+                SizedBox(height: 40,),
+                Text(
+                  "No Transaction available",
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(height: 20,),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Row(
                     children: [
-                      Card(
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                          color: Theme.of(context).primaryColor,
+                          width: 2,
+                        )),
+                        padding: EdgeInsets.all(10),
                         child: Text(
-                          userTransaction[index].title.toString(),
+                          '\$${userTransaction[index].amount.toStringAsFixed(2)}',
                           style: TextStyle(
-                            fontSize: 16,
                             fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Theme.of(context).primaryColor,
                           ),
                         ),
                       ),
-                      Card(
-                        child: Text(
-                          DateFormat.yMMMMd()
-                              .format(userTransaction[index].date),
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Card(
+                            child: Text(
+                              userTransaction[index].title.toString(),
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Card(
+                            child: Text(
+                              DateFormat.yMMMMd()
+                                  .format(userTransaction[index].date),
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ),
+                        ],
+                      )
                     ],
-                  )
-                ],
-              ),
-            );
-          },
-        itemCount: userTransaction.length,
-      ),
+                  ),
+                );
+              },
+              itemCount: userTransaction.length,
+            ),
     );
   }
 }
